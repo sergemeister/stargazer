614af9893cbf715d2548872072e7dd0f9846ec22
Sergey Kozhemiako <therealloker@gmail.com>
2020-08-27 15:14:05 +0300

    update test for logic

--- src/redux/logic/__tests__/reposDataHandlingLogic.spec.js
+++ src/redux/logic/__tests__/reposDataHandlingLogic.spec.js
@@ -2,114 +2,213 @@ import { fetchRepositoryLogic, fetchLanguagesLogic } from '../reposDataHandlingL
 import * as types from '../../../actions/types'
 
 describe('fetchRepositoryLogic', () => {
-  describe('using existing repository name', () => {
-    let dispatch
+  describe('transform', () => {
+    let reject
+    let next
 
-    beforeEach((done) => {
-      const httpClient = {
-        get(url) {
-          return new Promise((resolve, reject) => {
-            resolve({
-              data: {
+    describe('failure', () => {  
+      beforeEach((done) => {
+        reject = jest.fn(() => done())
+        next = jest.fn(() => done())
+        const action = {
+          type: types.GET_REPOSITORY,
+          payload: 'facebook123'
+        }
+  
+        fetchRepositoryLogic.transform({ action }, next, reject);
+      });
+  
+      it('should dispatch action GET_REPOSITORY_FAILURE with error message', () => {
+        expect(reject).toHaveBeenCalledTimes(1)
+        expect(reject).toHaveBeenCalledWith({
+          type: types.GET_REPOSITORY_FAILURE,
+          payload: "Invalid format"
+        })
+       })
+    })
+
+    describe('success', () => { 
+      beforeEach((done) => {
+        reject = jest.fn(() => done())
+        next = jest.fn(() => done())
+        const action = {
+          type: types.GET_REPOSITORY,
+          payload: 'facebook/react'
+        }
+  
+        fetchRepositoryLogic.transform({ action }, next, reject);
+      });
+  
+      it('should dispatch action GET_REPOSITORY', () => {
+        expect(next).toHaveBeenCalledTimes(1)
+        expect(next).toHaveBeenCalledWith({
+          type: types.GET_REPOSITORY,
+          payload: {
+            owner: 'facebook',
+            repo: 'react'
+          }
+        })
+      })
+    })
+  })
+
+  describe('process', () => {
+    describe('success', () => {
+      let dispatch
+
+      beforeEach((done) => {
+        const httpClient = {
+          get(url) {
+            return new Promise((resolve, reject) => {
+              resolve({
+                data: {
+                  id: 10270250,
+                  full_name: 'facebook/react',
+                  stargazers_count: 154386,
+                  subscribers_count: 6678,
+                  forks_count: 30270,
+                  clone_url: "https://github.com/facebook/react.git",
+                  languages_url: "https://api.github.com/repos/facebook/react/languages"
+                }
+              })
+            })
+          }
+        }
+  
+        dispatch = jest.fn(() => done());
+        const action = {
+          payload: {
+            owner: 'facebook',
+            repo: 'react'
+          }
+        }
+  
+        fetchRepositoryLogic.process({ httpClient, action }, dispatch);
+      })
+  
+      it('should dispatch action GET_REPOSITORY_SUCCESSFUL with repository data', () => {
+        expect(dispatch).toHaveBeenCalledTimes(1)
+        expect(dispatch).toHaveBeenCalledWith({
+          type: types.GET_REPOSITORY_SUCCESSFUL,
+          payload: {
+            id: 10270250,
+            full_name: "facebook/react",
+            stargazers_count: 154386,
+            subscribers_count: 6678,
+            forks_count: 30270,
+            clone_url: "https://github.com/facebook/react.git",
+            languages_url: "https://api.github.com/repos/facebook/react/languages"
+          }
+        })
+       })
+    })
+
+    describe('failure', () => {
+      let dispatch;
+  
+      beforeEach((done) => {
+        const httpClient = {
+          get(url) {
+            return new Promise((resolve, reject) => {
+              reject(new Error('not found 404'));
+            });
+          }
+        };
+  
+        dispatch = jest.fn(() => done());
+        const action = {
+          payload: {
+            owner: 'jasflkja322',
+            repo: 'react'
+          }
+        }
+  
+        fetchRepositoryLogic.process({ httpClient, action }, dispatch);
+      });
+  
+      it('should dispatch action GET_REPOSITORY_FAILURE with error message', () => {
+        expect(dispatch).toHaveBeenCalledTimes(1)
+        expect(dispatch).toHaveBeenCalledWith({
+          type: types.GET_REPOSITORY_FAILURE,
+          payload: "Repository not found"
+        })
+       });
+    });
+  })
+})
+
+describe('fetchRepositoryLanguagesLogic', () => {
+  describe('transform', () => {
+    let reject
+    let next
+  
+    describe('success', () => {
+      beforeEach((done) => {
+        reject = jest.fn(() => done())
+        next = jest.fn(() => done())
+
+        const action = {
+          type: types.GET_REPOSITORY_LANGUAGES,
+          payload: "10270250"
+        }
+
+        const getState = () => ({
+          repositories: {
+            data: {
+              "10270250": {
                 id: 10270250,
-                full_name: 'facebook/react',
+                full_name: "facebook/react",
                 stargazers_count: 154386,
                 subscribers_count: 6678,
                 forks_count: 30270,
                 clone_url: "https://github.com/facebook/react.git",
                 languages_url: "https://api.github.com/repos/facebook/react/languages"
               }
-            })
-          })
-        }
-      }
+            }
+          }
+        })
+  
+        fetchLanguagesLogic.transform({ action, getState }, next, reject);
+      });
 
-      dispatch = jest.fn(() => done());
-      const action = {
-        payload: {
-          owner: 'facebook',
-          repo: 'react'
-        }
-      }
-
-      fetchRepositoryLogic.process({ httpClient, action }, dispatch);
+      it('should dispatch action GET_REPOSITORY_LANGUAGES', () => {
+        expect(next).toHaveBeenCalledTimes(1)
+        expect(next).toHaveBeenCalledWith({
+          type: types.GET_REPOSITORY_LANGUAGES,
+          payload: {
+            id: '10270250',
+            languages_url: "https://api.github.com/repos/facebook/react/languages"
+          }
+        })
+      })
     })
 
-    it('should dispatch action GET_REPOSITORY_SUCCESSFUL with repository data', () => {
-      expect(dispatch).toHaveBeenCalledTimes(1)
-      expect(dispatch).toHaveBeenCalledWith({
-        type: types.GET_REPOSITORY_SUCCESSFUL,
-        payload: {
-          id: 10270250,
-          full_name: "facebook/react",
-          stargazers_count: 154386,
-          subscribers_count: 6678,
-          forks_count: 30270,
-          clone_url: "https://github.com/facebook/react.git",
-          languages_url: "https://api.github.com/repos/facebook/react/languages"
+    describe('failure', () => {
+      beforeEach((done) => {
+        reject = jest.fn(() => done())
+        next = jest.fn(() => done())
+        
+        const action = {
+          type: types.GET_REPOSITORY_LANGUAGES,
+          payload: "10270250"
         }
+
+        const getState = () => ({
+          repositories: {
+            data: {}
+          }
+        })
+  
+        fetchLanguagesLogic.transform({ action, getState }, next, reject);
+      });
+
+      it('should reject an action', () => {
+        expect(reject).toHaveBeenCalledTimes(1)
       })
-     })
+    })
   })
 
-  describe('repository not found', () => {
-    let dispatch;
-
-    beforeEach((done) => {
-      const httpClient = {
-        get(url) {
-          return new Promise((resolve, reject) => {
-            reject(new Error('not found 404'));
-          });
-        }
-      };
-
-      dispatch = jest.fn(() => done());
-      const action = {
-        payload: {
-          owner: 'jasflkja322',
-          repo: 'react'
-        }
-      }
-
-      fetchRepositoryLogic.process({ httpClient, action }, dispatch);
-    });
-
-    it('should dispatch action GET_REPOSITORY_FAILURE with error message', () => {
-      expect(dispatch).toHaveBeenCalledTimes(1)
-      expect(dispatch).toHaveBeenCalledWith({
-        type: types.GET_REPOSITORY_FAILURE,
-        payload: "Repository not found"
-      })
-     });
-  });
-
-  describe('invalid format', () => {
-    let reject
-    let next
-
-    beforeEach((done) => {
-      reject = jest.fn(() => done())
-      next = (act) => done(act)
-      const action = {
-        payload: 'facebook123'
-      }
-
-      fetchRepositoryLogic.transform({ action }, next, reject);
-    });
-
-    it('should dispatch action GET_REPOSITORY_FAILURE with error message', () => {
-      expect(reject).toHaveBeenCalledTimes(1)
-      expect(reject).toHaveBeenCalledWith({
-        type: types.GET_REPOSITORY_FAILURE,
-        payload: "Invalid format"
-      })
-     })
-  })
-})
-
-describe('fetchRepositoryLanguagesLogic', () => {
-  describe('successful request', () => {
+  describe('process success', () => {
     let dispatch
 
     beforeEach((done) => {
